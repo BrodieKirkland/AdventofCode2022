8a2ef67495676f03fcec744447cf53b72f7e6fef
Brodie <brodie.kirkland@my.jcu.edu>
2022-12-02 23:27:44 +1000

    Add complete day_01 part two

--- day_01/calorie_counting.py
+++ day_01/calorie_counting.py
@@ -6,6 +6,11 @@ with open("calories.txt") as infile:
     data = calorie_collection.split("\n\n")
     elves = [elf.split() for elf in data]
     elves = [[int(calorie) for calorie in elf] for elf in elves]
-    highest_calories = max([sum(elf) for elf in elves])
-    print(highest_calories)
-
+    total_calories_per_elf = [sum(elf) for elf in elves]
+    total = 0
+    for i in range(3):
+        highest_calorie = max(total_calories_per_elf)
+        print("Max: ", highest_calorie)
+        calorie = total_calories_per_elf.pop(total_calories_per_elf.index(highest_calorie))
+        total += calorie
+    print("Total:", total)
