e0042bf823bef19b048c3528a379c6cade475f45
Brodie <brodie.kirkland@my.jcu.edu>
2022-12-02 23:07:42 +1000

    Add complete day_01

--- day_01/calorie_counting.py
+++ day_01/calorie_counting.py
@@ -1,3 +1,11 @@
 """Advent of code 2022 day 1"""
 
+
 with open("calories.txt") as infile:
+    calorie_collection = infile.read()
+    data = calorie_collection.split("\n\n")
+    elves = [elf.split() for elf in data]
+    elves = [[int(calorie) for calorie in elf] for elf in elves]
+    highest_calories = max([sum(elf) for elf in elves])
+    print(highest_calories)
+
