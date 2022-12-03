cee350ddbd69e55c063fa12934d7cc60a61ad776
Brodie <brodie.kirkland@my.jcu.edu>
2022-12-03 21:53:24 +1000

    Add complete day_03 part two

--- day_03/rucksacks.py
+++ day_03/rucksacks.py
@@ -8,18 +8,33 @@ for i, letter in enumerate(alphabet, 1):
     alphabet_to_values[letter.upper()] = i + 26
 
 with open("puzzle_input.txt") as infile:
-    rucksack_collection = []
+    rucksacks = []
+    elf_groups = []
+    elves = []
     for line in infile:
         line = line.strip()
+        elves.append(line)
         halfway = int(len(line) / 2)
-        rucksacks = [line[:halfway], line[halfway:]]
-        rucksack_collection.append(rucksacks)
+        compartments = [line[:halfway], line[halfway:]]
+        rucksacks.append(compartments)
+    for i in range(0, len(elves), 3):
+        elf_groups.append(elves[i:i+3])
 duplicates = []
-for rucksacks in rucksack_collection:
-    duplicate = [item for item in rucksacks[0] if item in rucksacks[1]]
+for compartments in rucksacks:
+    duplicate = [item for item in compartments[0] if item in compartments[1]]
     duplicates.append(duplicate[0])
 total = 0
 for letter in duplicates:
     number = alphabet_to_values[letter]
     total += number
-print(total)
+group_duplicates = []
+group_total = 0
+for group in elf_groups:
+    group_duplicate = [item for item in group[0] if item in group[1] and item in group[2]]
+    group_duplicates.append(group_duplicate[0])
+for letter in group_duplicates:
+    number = alphabet_to_values[letter]
+    group_total += number
+print(f"Total: {total}")
+print(f"Group total: {group_total}")
+
