d7a875506319ce6e074698310f68349ca68e1135
Brodie <brodie.kirkland@my.jcu.edu>
2022-12-20 11:20:34 +1000

    Add complete day 6 part two

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -1,16 +1,16 @@
 """Advent of Code day 6"""
 
 with open("puzzle_input.txt") as infile:
-    is_four_unique_nums = False
+    is_unique_nums = False
     numbers = infile.readline()
-    while not is_four_unique_nums:
-        quad = []
+    while not is_unique_nums:
+        group = []
         for i in range(len(numbers)):
-            quad = numbers[i:i+4]
-            if len(set(quad)) == 4:
-                is_four_unique_nums = True
+            group = numbers[i:i + 14]
+            if len(set(group)) == 14:
+                is_unique_nums = True
                 index = i
                 break
-    numbers = numbers[0:i+4]
+    numbers = numbers[0:i+14]
     print(len(numbers))
 
