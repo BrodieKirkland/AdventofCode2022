493d4abaabea94380f7edf06199ce11eda7325c8
Brodie <brodie.kirkland@my.jcu.edu>
2022-12-20 12:05:50 +1000

    Refactor group_size variable

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -1,16 +1,17 @@
 """Advent of Code day 6"""
 
+group_size = 14
 with open("puzzle_input.txt") as infile:
     is_unique_nums = False
     numbers = infile.readline()
     while not is_unique_nums:
         group = []
         for i in range(len(numbers)):
-            group = numbers[i:i + 14]
-            if len(set(group)) == 14:
+            group = numbers[i:i + group_size]
+            if len(set(group)) == group_size:
                 is_unique_nums = True
                 index = i
                 break
-    numbers = numbers[0:i+14]
+    numbers = numbers[0:i+group_size]
     print(len(numbers))
 
